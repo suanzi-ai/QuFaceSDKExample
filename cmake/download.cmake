3bcc968e800ac52d81feeb55088dcf1b8eb26995
Xu Zhipei <xuzhipei@gmail.com>
2020-05-13 10:11:54 +0800

    chore(cmake): update cmake lib

--- cmake/download.cmake
+++ cmake/download.cmake
@@ -7,29 +7,29 @@ function(download_file url filename hash_type hash)
 endfunction()
 
 function(download_and_extract url filename hash_type hash extract_dir)
+  if(EXISTS ${extract_dir})
+    message(STATUS "${extract_dir} already exists, skip download & extract")
+    return()
+  endif()
+
   if(NOT EXISTS ${filename})
-
     download_file(${url} ${filename} ${hash_type} ${hash})
-
   else()
-
     file(${hash_type} ${filename} _CHKSUM)
     if(NOT ${_CHKSUM} STREQUAL ${hash})
       message(WARNING "File hash miss match ...")
       file(REMOVE ${filename})
       download_file(${url} ${filename} ${hash_type} ${hash})
     endif()
-
-  endif()
-
-  if(EXISTS ${extract_dir})
-    file(REMOVE_RECURSE ${extract_dir})
   endif()
 
   message(STATUS "Extract ${filename} to ${extract_dir} ...")
 
   set(temp_dir ${CMAKE_BINARY_DIR}/tmp_for_extract.dir)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${temp_dir})
+  if(EXISTS ${temp_dir})
+    file(REMOVE_RECURSE ${temp_dir})
+  endif()
+  file(MAKE_DIRECTORY ${temp_dir})
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf ${filename}
                   WORKING_DIRECTORY ${temp_dir})
 
@@ -40,7 +40,7 @@ function(download_and_extract url filename hash_type hash extract_dir)
   endif()
 
   get_filename_component(contents ${contents} ABSOLUTE)
-  file(RENAME ${contents} ${extract_dir})
+  file(INSTALL "${contents}/" DESTINATION ${extract_dir})
   file(REMOVE_RECURSE ${temp_dir})
 
 endfunction()
--- cmake/himix200.toolchain.cmake
+++ cmake/himix200.toolchain.cmake
@@ -2,11 +2,13 @@
 # cannot recognized.
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
+
 # when hislicon SDK was installed, toolchain was installed in the path as below:
-set(CMAKE_C_COMPILER
-    /opt/hisi-linux/x86-arm/arm-himix200-linux/bin/arm-himix200-linux-gcc)
-set(CMAKE_CXX_COMPILER
-    /opt/hisi-linux/x86-arm/arm-himix200-linux/bin/arm-himix200-linux-g++)
+set(CROSS_PREFIX
+    /opt/hisi-linux/x86-arm/arm-himix200-linux/bin/arm-himix200-linux-)
+set(CROSS_HOST arm-himix200-linux)
+set(CMAKE_C_COMPILER ${CROSS_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_PREFIX}g++)
 
 # set searching rules for cross-compiler
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -21,3 +23,8 @@ set(CROSS_COMPILATION_ARCHITECTURE armv7-a)
 set(CMAKE_CXX_FLAGS
     "-std=c++11 -march=armv7-a -mfloat-abi=softfp -mfpu=neon-vfpv4 -fopenmp ${CMAKE_CXX_FLAGS}"
 )
+
+# other settings
+set(CMAKE_CXX_FLAGS "-DCV__EXCEPTION_PTR=0 ${CMAKE_CXX_FLAGS}")
+
+add_definitions(-D__HISI__)
